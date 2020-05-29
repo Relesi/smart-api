2b9d14796986cb5e1bfec628cd57a67c192ebb07
x212498 <renatolessa.2011@hotmail.com>
2020-05-29 17:29:34 -0300

    first structure

---
+++ src/main/java/com/relesi/smart/api/enums/PerfilEnum.java
@@ -0,0 +1,1 @@
+package com.relesi.smart.api.enums;public enum PerfilEnum {    ROLE_ADMIN,    ROLE_USUARIO;}
---
+++ src/main/java/com/relesi/smart/api/repositories/EmpresaRepository.java
@@ -0,0 +1,1 @@
+package com.relesi.smart.api.repositories;import com.relesi.smart.api.entities.Empresa;import org.springframework.data.jpa.repository.JpaRepository;import org.springframework.transaction.annotation.Transactional;public interface EmpresaRepository extends JpaRepository<Empresa, Long> {    @Transactional(readOnly = true)    Empresa findByCnpj(String cnpj);}
