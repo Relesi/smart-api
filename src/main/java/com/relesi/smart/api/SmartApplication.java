ecfd4afe334c3569e1bc7da465aab4e92dbe0605
relesi <renatolessa.2011@hotmail.com>
2020-07-20 16:55:06 -0300

    authorization  to implementation and authentication to token JWT

--- src/main/java/com/relesi/smart/api/SmartApplication.java
+++ src/main/java/com/relesi/smart/api/SmartApplication.java
@@ -2,8 +2,10 @@ package com.relesi.smart.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SmartApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/relesi/smart/api/security/services/JwtUserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package com.relesi.smart.api.security.services;
+
+import java.util.Optional;
+
+import com.relesi.smart.api.entities.Funcionario;
+import com.relesi.smart.api.security.JwtUserFactory;
+import com.relesi.smart.api.services.FuncionarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class JwtUserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private FuncionarioService funcionarioService;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<Funcionario> funcionario = funcionarioService.buscarPorEmail(username);
+
+		if (funcionario.isPresent()) {
+			return JwtUserFactory.create(funcionario.get());
+		}
+
+		throw new UsernameNotFoundException("Email não encontrado.");
+	}
+
+}
